e5983927dc024fdbf1b408db20aef582e771a4d7
Gurps Bassi <gurpiar.bassi@gmail.com>
2014-12-17 23:04:45 +0000

    repository impl

--- SpringRest/src/main/java/com/gurps/library/repository/LibraryRepository.java
+++ SpringRest/src/main/java/com/gurps/library/repository/LibraryRepository.java
@@ -8,4 +8,18 @@ import com.gurps.library.domain.Book;
 @Repository
 public interface LibraryRepository extends MongoRepository<Book, String> {
 	
+	/**
+	 * By adding the annotation org.springframework.data.mongodb.repository.Query repository finder methods you can specify a MongoDB JSON query string to use instead of having the query derived from the method name. For example
+
+public interface PersonRepository extends MongoRepository<Person, String>
+
+  @Query("{ 'firstname' : ?0 }")
+  List<Person> findByThePersonsFirstname(String firstname);
+
+}
+
+
+to restrict the properties returned. do @Query(value="{ 'firstname' : ?0 }", fields="{ 'firstname' : 1, 'lastname' : 1}")
+	 */
+	
 }
