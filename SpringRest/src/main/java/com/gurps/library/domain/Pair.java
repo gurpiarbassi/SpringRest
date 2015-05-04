6f139d0e5dba2477f85b517b40ff1c0f48272327
Gurpiar Bassi <gurpiarbassi@Gurpiars-MacBook-Pro.local>
2015-05-04 17:36:43 +0100

    useful class to return pair of values.

---
+++ SpringRest/src/main/java/com/gurps/library/domain/Pair.java
@@ -0,0 +1,25 @@
+package com.gurps.library.domain;
+
+/**
+ * Generic Pair class. Useful when you want to return a pair from a method.
+ * 
+ * @author gurpiarbassi
+ *
+ */
+public class Pair<T1, T2> {
+	private final T1 value1;
+	private final T2 value2;
+
+	public Pair(T1 value1, T2 value2) {
+		this.value1 = value1;
+		this.value2 = value2;
+	}
+
+	public T1 getValue1() {
+		return value1;
+	}
+
+	public T2 getValue2() {
+		return value2;
+	}
+}
